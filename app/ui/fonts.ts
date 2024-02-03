97dfd0bb311898b3d99fb7b40300c7227e847a54
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2024-02-02 20:22:45 -0500

    initial comment

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Roboto_Mono } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
+//export const honk = Honk({ weight: ['400', '700'], subsets: ['latin'] });
